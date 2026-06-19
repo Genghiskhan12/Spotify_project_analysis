7b5fb14f6ac4f614910da9d7d2725d140f920be9
Chingiz Yusupov <usupovcingiz51@gmail.com>
2026-06-19 12:07:24 +0500

    Update 03_behavior_analysis.sql

--- Spotify_project/sql/03_behavior_analysis.sql
+++ Spotify_project/sql/03_behavior_analysis.sql
@@ -97,3 +97,29 @@ GROUP BY music_recc_rating
 ORDER BY total_count DESC;
 
 -- Most users rated Spotify recommendations with scores of 3 and 4.
+
+-- Podcast listening frequency
+
+SELECT 
+    pod_lis_frequency,
+    COUNT(*) AS total_count,
+    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM spotify_data), 1) AS percentage
+FROM spotify_data
+GROUP BY pod_lis_frequency
+ORDER BY total_count DESC;
+
+-- Most users listen to podcasts rarely or never.
+-- Only a smaller portion of users listen to podcasts weekly or several times a week.
+
+-- Favorite podcast genres
+
+SELECT 
+    fav_pod_genre,
+    COUNT(*) AS total_count,
+    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM spotify_data), 1) AS percentage
+FROM spotify_data
+GROUP BY fav_pod_genre
+ORDER BY total_count DESC;
+
+-- Most users do not have a specific favorite podcast genre.
+-- Comedy and lifestyle & health podcasts are among the most popular categories.
